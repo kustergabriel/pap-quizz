4b2138ab5c1900fdac181f8129a0d61054c5d5c4
Gabriel Kuster <gabrielazevedo1812@gmail.com>
2026-03-03 14:45:59 -0300

    algumas funcoes de seguranca adicionadas

--- back-end/server.js
+++ back-end/server.js
@@ -3,7 +3,6 @@ import app from '../src/app.js'
 
 const PORT = 3000 // Porta em que sera hospedado
 
-
 app.listen(PORT, () => {
     console.log("Escutando!")
 })
--- src/app.js
+++ src/app.js
@@ -1,27 +1,44 @@
 import express from 'express'
+import csrf from 'csurf';
 import routes from '../src/routes/index.js'; 
 import path from 'path';
 import conctaNaDatabase from '../back-end/config/dbconnect.js'
 import { fileURLToPath } from 'url';
 import session from "express-session";
+import rateLimit from 'express-rate-limit';
+import cookieParser from 'cookie-parser';
 
 const conexao = await conctaNaDatabase()
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
+const csrfProtection = csrf({ cookie: true });
 
 conexao.on('error', (erro)=> {
     console.error("erro de conexao ", erro);
 })
 
+const globalLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 100, 
+    message: { message: "Muitas requisições deste IP. Tente novamente mais tarde." }
+});
+
 app.use(session({
     secret: process.env.SECRET, 
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false } // Em produção com HTTPS, usar true
+    cookie: { 
+    secure: process.env.SECURE === 'production',
+    httpOnly: true,
+    sameSite: 'strict'
+            }
 }));
 
+app.use(cookieParser());
+app.use(globalLimiter); 
 app.use(express.json());
+app.use(csrfProtection);
 app.use(express.static(path.join(__dirname, '../public')));
 
 routes(app);
